ae2bfe39fd174f79f30179aa142bd67a67ded489
Philippe-S01 <philippe.servant@epfl.ch>
2023-05-28 23:25:30 +0200

    visualization update \ je suis trop fort. \ (predictions ajoutés (avec alpha en fonction de la prob) manque légende et dégradé sur le temps (gt + pred))

---
+++ apply_model.py
@@ -0,0 +1,72 @@
+import torch
+import sys
+import pickle
+import numpy as np
+
+sys.path.append("MTR/tools")
+
+from train import parse_config
+
+from mtr.models import model as model
+from mtr.utils import common_utils
+from mtr.datasets import build_dataloader
+
+# --cfg_file MTR/tools/cfgs/waymo/mtr_single_file_vis.yaml
+
+# checkpoint_path = "MTR\output\tools\cfgs\waymo\mtr_weak\my_first_exp"
+checkpoint_path = "../checkpoint_epoch_1.pth"
+data_path = "../data"
+data_filename = "sample_867dd000677d389.pkl"
+
+def load_model():
+    # checkpoint = torch.load(checkpoint_path)
+    args, cfg = parse_config()
+    logger = common_utils.create_logger("log.txt", rank=cfg.LOCAL_RANK)  # create logger
+    model_ = model.MotionTransformer(config=cfg.MODEL)
+    model_.load_params_from_file(checkpoint_path, logger=logger, to_cpu=False)
+    model_.eval()
+    return model_
+
+def dataloader():
+    args, cfg = parse_config()
+    logger = common_utils.create_logger("log_vis.txt", rank=cfg.LOCAL_RANK)  # create logger
+    dataset, dataloader_, sampler = build_dataloader(
+        dataset_cfg=cfg.DATA_CONFIG,
+        batch_size=cfg.OPTIMIZATION.BATCH_SIZE_PER_GPU,
+        dist=False, workers=1, logger=logger,
+        training=False, merge_all_iters_to_one_epoch=False, total_epochs=cfg.OPTIMIZATION.NUM_EPOCHS,
+        add_worker_init_fn=True
+    )
+    return dataset, dataloader_, sampler
+
+def create_pickle(filename):
+    data_file = open(data_path + "/processed_scenarios_training/" + filename, 'rb')
+    data = pickle.load(data_file)
+    data_file.close()
+    # create a dict with these keys 'scenario_id', 'timestamps_seconds', 'current_time_index', 'sdc_track_index', 'objects_of_interest', 'tracks_to_predict'
+    # and values from data
+    dict = {}
+    dict['scenario_id'] = data['scenario_id']
+    dict['timestamps_seconds'] = data['timestamps_seconds']
+    dict['current_time_index'] = data['current_time_index']
+    dict['sdc_track_index'] = data['sdc_track_index']
+    dict['objects_of_interest'] = data['objects_of_interest']
+    dict['tracks_to_predict'] = data['tracks_to_predict']
+    list = [dict]
+    pickle.dump(list, open(data_path + "/processed_scenarios_single_file_vis.pkl", 'wb'))
+
+
+
+def apply_model(model_):
+    dataset, dataloader_, sampler = dataloader()
+    i, batch_dict = next(enumerate(dataloader_))
+    with torch.no_grad():
+        batch_pred_dicts = model_(batch_dict)
+        final_pred_dicts = dataset.generate_prediction_dicts(batch_pred_dicts, output_path=None)
+    return final_pred_dicts
+
+def apply(data_filename=data_filename):
+    model_ = load_model().cuda()
+    create_pickle(data_filename)
+    result = apply_model(model_)
+    return result
--- visualize.py
+++ visualize.py
@@ -9,13 +9,19 @@ sys.path.append("MTR/mtr/datasets/waymo/")
 
 from waymo_types import polyline_type
 
+from apply_model import apply
+
+# launch command : python visualize.py --cfg_file MTR/tools/cfgs/waymo/mtr_single_file_vis.yaml
+
 # fichiers importants
 # https://github.com/tudoroancea/civil_459_project/blob/main/MTR/mtr/datasets/waymo/waymo_dataset.py#L335
 # https://github.com/tudoroancea/civil_459_project/blob/main/MTR/mtr/datasets/waymo/data_preprocess.py
 # https://github.com/tudoroancea/civil_459_project/blob/main/MTR/mtr/datasets/waymo/waymo_types.py
 
 # file = "../data/processed_scenarios_training/sample_2c25255b942748dc.pkl"
-file = "../data/processed_scenarios_training/sample_867dd000677d389.pkl"
+path = "../data/processed_scenarios_training/"
+filename = "sample_867dd000677d389.pkl"
+file = path + filename
 # file = "../data/processed_scenarios_training/sample_867dd000677d389.pkl"
 
 
@@ -82,8 +88,16 @@ def plot_frame(ax, data, frame):
         road_line_polylines = data["map_infos"]["all_polylines"][indices[0]:indices[1]]
         ax.plot(road_line_polylines[:, 0], road_line_polylines[:, 1], color='lightgray', linewidth=0.2)
 
-    ax.set_aspect('equal')
-    ax.set_box_aspect(1)
+
+def get_prediction():
+    return apply(filename)
+
+def add_prediction_to_plot(ax, pred):
+    for i in pred:
+        trajs = i["pred_trajs"]
+        num_trajs = trajs.shape[0]
+        for j in range(num_trajs):
+            plt.scatter(trajs[j, :, 0], trajs[j, :, 1], color='green', s=0.1, alpha=i["pred_scores"][j])
 
 with open(file, 'rb') as f:
     data = pickle.load(f)
@@ -96,16 +110,20 @@ with open(file, 'rb') as f:
 
     fig, ax = plt.subplots()
 
+    pred = get_prediction()
+
     def animate_func(j):
         ax.clear()
         plot_frame(ax, data, j)
+        add_prediction_to_plot(ax, pred)
 
     # animate the trajectories with rectangles    
     # the animation should last 8 seconds 
     interval = 1000/(data["track_infos"]["trajs"].shape[1] / 8)
     anim = FuncAnimation(fig, animate_func, frames=range(0, data["track_infos"]["trajs"].shape[1]), interval=interval)
     
-
+    ax.set_aspect('equal')
+    ax.set_box_aspect(1)
     # play the animation
     anim.save('animation.gif', writer='pillow', fps=30, dpi=750)
     
